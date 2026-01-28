3e185afc3e40b143370a256d00e898f97302cdbc
lordbyaku <lordbyaku@gmail.com>
2026-01-28 17:17:09 +0700

    fix pdf

--- sync.bat
+++ sync.bat
@@ -1,18 +1,19 @@
 @echo off
 echo === Sinkronisasi GitHub ===
-echo 1. Menarik perubahan terbaru dari server...
-git pull origin main --rebase
 
-echo.
-echo 2. Menyiapkan perubahan lokal...
+echo 1. Menyiapkan perubahan lokal...
 git add .
 
 set /p commit_msg="Masukkan pesan commit (tekan Enter untuk default 'update'): "
 if "%commit_msg%"=="" set commit_msg=update
 
 echo.
-echo 3. Menyimpan perubahan...
+echo 2. Menyimpan perubahan...
 git commit -m "%commit_msg%"
+
+echo.
+echo 3. Menarik perubahan terbaru dari server...
+git pull origin main --rebase
 
 echo.
 echo 4. Mengirim ke GitHub...
